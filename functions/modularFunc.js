960e1b0b881b0ce5699f091b878127b017ef68db
sabohi <sabuhizaidi616@gmail.com>
2019-10-10 12:34:58 +0530

    break before each case - bug fixed

--- functions/modularFunc.js
+++ functions/modularFunc.js
@@ -1,5 +1,4 @@
 const fetchCall = async (url, options, type) =>
-<<<<<<< HEAD:components/fetchCaller.js
 {
   const response = await fetch(url, options);
   switch(type)
@@ -9,30 +8,12 @@ const fetchCall = async (url, options, type) =>
     break;
     case "json":
       return response.json();  
-    break; 
+    break;
     default :
-      return response.text();
+    return response.text();
   }
 };
-export default fetchCall;
-=======
-    {
-      const response = await fetch(url, options);
-      switch(type)
-      {
-        case "text":
-          return response.text();
-          
-        case "json":
-          return response.json();  
-          
-        default :
-        return response.text();
-        
-      }
-    };
     
 export {
     fetchCall
 };
->>>>>>> 3acc91ab47e06756ca76a5016e3494f61a48b42e:functions/modularFunc.js

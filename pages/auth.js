71d3d12713c40877b4013d14b1b59d9d02bb8c00
sh88688 <sh88688@gmail.com>
2019-10-14 18:40:12 +0530

    minor in auth

--- pages/auth.js
+++ pages/auth.js
@@ -5,6 +5,7 @@ import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, M
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import "../static/css/auth.css";
 
 class  AuthPage extends Component  {
   
@@ -14,73 +15,62 @@ class  AuthPage extends Component  {
   }
   clickHandler = (event) => {
     event.preventDefault(); 
-    window.location="/dashboard";
-    // const url = new URL(`http://api.cz-tuts.com/login`);
-    // const reqBody={
-    //   email: this.state.email,
-    //   password: this.state.password
-    // };
-    // const fetchCallOptions = {
-    //       body: JSON.stringify(reqBody),
-    //       method:'post',
-    //       headers: {
-    //       'Content-Type': 'application/json' 
-    //     },
-    //     credentials:'include'
-    // };
-    // //API CALL:
-    // const result = fetchCall(url, fetchCallOptions, "json");   
-    // result.then(function(data){
-    //     if(data.status){
-    //         // toaster.success("Successfully Logged In");
-    //         document.cookie='nodeSessID='+data.nodeSessID+";maxAge=3600000;path='/';domain='cz-tuts.com'";
-    //         window.location="/dashboard";
-    //     }
-    //     else{
-    //       alert(data.message);
-    //     }
-    // });
+    const url = new URL(`http://api.cz-tuts.com/login`);
+    const reqBody={
+      email: this.state.email,
+      password: this.state.password
+    };
+    const fetchCallOptions = {
+          body: JSON.stringify(reqBody),
+          method:'post',
+          headers: {
+          'Content-Type': 'application/json' 
+        },
+        credentials:'include'
+    };
+    //API CALL:
+    const result = fetchCall(url, fetchCallOptions, "json");   
+    result.then(function(data){
+        if(data.status){
+            // toaster.success("Successfully Logged In");
+            document.cookie='nodeSessID='+data.nodeSessID+";maxAge=3600000;path='/';domain='cz-tuts.com'";
+            window.location="/dashboard";
+        }
+        else{
+          alert(data.message);
+        }
+    });
   }
    inputHandler = (event) =>{
     this.setState({[event.target.name]:event.target.value});
   }
   render(){
+    const Input = [{label:"Your Email",type:"email",name:"email"},{label:"Your Password",type:"password",name:"password"}].map(fields => (
+      <MDBInput
+        label={fields.label}
+        group
+        type={fields.type}
+        error="wrong"
+        success="right"
+        name ={fields.name}
+        onChange={this.inputHandler}
+        required
+      />
+    ));
+
   return (
     <MDBContainer >
       <MDBRow center="true" >
         <MDBCol md="5">
-          <MDBCard >
-            <style>{`
-            .card{margin-top:20%}
-            `}</style>
+          <MDBCard className="mt-5">
             <MDBCardBody className="mx-4">
               <div className="text-center">
                <img src="static/img/CZentrixLogo.png" height="50" width="150"></img>
               </div>
-              <MDBInput
-                label="Your email"
-                group
-                type="email"
-                error="wrong"
-                success="right"
-                name ="email"
-                onChange={this.inputHandler}
-                required
-              />
-              <MDBInput
-                label="Your password"
-                group
-                type="password"
-                name="password"
-                validate
-                containerClass="mb-0"
-                onChange={this.inputHandler}
-                required
-              />
+              {Input}
               <p className="font-small blue-text d-flex justify-content-end pb-3">
                 Forgot
                 <a href="#!" className="blue-text ml-1">
-
                   Password?
                 </a>
               </p>
@@ -143,24 +133,6 @@ class  AuthPage extends Component  {
          
         </MDBCol>
       </MDBRow>
-     
-      <style global jsx>{`
-      
-      .left-card-login{
-        margin-top:40%
-      }
-        .form-elegant .font-small {
-            font-size: 0.8rem; }
-          
-          .form-elegant .z-depth-1a {
-            -webkit-box-shadow: 0 2px 5px 0 rgba(55, 161, 255, 0.26), 0 4px 12px 0 rgba(121, 155, 254, 0.25);
-            box-shadow: 0 2px 5px 0 rgba(55, 161, 255, 0.26), 0 4px 12px 0 rgba(121, 155, 254, 0.25); }
-          
-          .form-elegant .z-depth-1-half,
-          .form-elegant .btn:hover {
-            -webkit-box-shadow: 0 5px 11px 0 rgba(85, 182, 255, 0.28), 0 4px 15px 0 rgba(36, 133, 255, 0.15);
-            box-shadow: 0 5px 11px 0 rgba(85, 182, 255, 0.28), 0 4px 15px 0 rgba(36, 133, 255, 0.15); }
-      `}</style>
     </MDBContainer>
   );
 }
